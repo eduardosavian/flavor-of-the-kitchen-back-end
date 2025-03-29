0ee788bd03b3ba5a0d05cd2abe0fec1b33b79db2
eduardosavian <eduardosavian@outlook.com>
2025-03-28 23:06:42 -0300

    Exclude trash code

--- req.sh
+++
@@ -1,4 +0,0 @@
-curl http://localhost:8000 \
-	-H "Content-Type: application/json" \
-	-d '{ "name": "Eduardo Savian de Oliveira", "email": "eduardosaviandeoliveira@gmail.com", "password": "69696969"}'
-
